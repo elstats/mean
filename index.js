4d48fc7dce09ad7aa6b86895372e783234521f3f
Michał Jezierski <ambinanitelo@gmail.com>
2018-03-02 20:27:23 +0100

    eslint fixes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
 function add(a, b) {
-  return a + b;
+    return a + b;
 }
 
 module.exports = {
-  calc: function (data) {
-    return data.reduce(add, 0) / data.length;
-  }
+    calc (data) {
+        return data.reduce(add, 0) / data.length;
+    }
 };
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import { calc } from './index';
 
 test('should calculate mean', t => {
-  const mean = calc([1, 10, 100, 45, -4, 0.5, -16]);
+    const mean = calc([1, 10, 100, 45, -4, 0.5, -16]);
 
-  t.is(mean, 19.5);
+    t.is(mean, 19.5);
 });
